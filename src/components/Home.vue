7c902acf9bb1fda8628a5b19a3abe0700514aa98
Aniket-Metapercept <97732091+Aniket-Metapercept@users.noreply.github.com>
2022-01-27 15:56:15 +0530

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <Header />
+ <h1>Hello, Welcome to Home Page</h1>
+
+  <div v-if="src_img">
+    <img :src="src_img" alt="">
+  </div>
+
+</template>
+
+<script>
+
+
+import Header from './Header.vue'
+export default {
+  name: 'Home',
+  props: {
+
+  },
+  data(){
+    return {
+      src_img: localStorage.getItem("img")
+    }
+  },
+  components:{
+    Header
+  },
+   mounted(){
+      let user = localStorage.getItem('user')
+      if(!user){
+          this.$router.push({name:"signup"}) 
+      }
+  }
+ 
+}
+</script>
